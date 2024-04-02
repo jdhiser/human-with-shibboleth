380e4cc0b6f2229c945c3f15eaeaa76ccdc73e02
jdhiser <jdhiser@gmail.com>
2024-04-02 15:09:29 +0000

    Working shib workflow?

--- pyhuman/app/workflows/browse_shibboleth.py
+++ pyhuman/app/workflows/browse_shibboleth.py
@@ -1,5 +1,4 @@
 from time import sleep
-import os
 import random
 
 # from soupsieve import select
@@ -7,19 +6,14 @@ import random
 from ..utility.base_workflow import BaseWorkflow
 from ..utility.webdriver_helper import WebDriverHelper
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.support.wait import WebDriverWait
-from selenium.common.exceptions import ElementNotInteractableException
+from selenium.webdriver.common.action_chains import ActionChains
 
 WORKFLOW_NAME = 'ShibbolethBrowser'
 WORKFLOW_DESCRIPTION = 'Browse a website secured by Shibboleth'
 
 DEFAULT_INPUT_WAIT_TIME = 2
-MIN_WATCH_TIME = 2 # Minimum amount of time to watch a video, in seconds
-MAX_WATCH_TIME = 150 # Maximum amount of time to watch a video, in seconds
 MIN_WAIT_TIME = 2 # Minimum amount of time to wait after searching, in seconds
 MAX_WAIT_TIME = 5 # Maximum amount of time to wait after searching, in seconds
-MAX_SUGGESTED_VIDEOS = 10
 
 SEARCH_LIST = 'browse_youtube.txt'
 
@@ -35,16 +29,68 @@ class ShibbolethBrowse(BaseWorkflow):
 
         self.input_wait_time = input_wait_time
 
+
+
     def action(self, extra=None):
         self.sign_in()
 
     """ PRIVATE """
 
     def sign_in(self):
-        random_search = self._get_random_search()
-
         # Navigate to youtube
         self.driver.driver.get('https://service.castle.os/secure/index.html')
         sleep(random.randrange(MIN_WAIT_TIME, MAX_WAIT_TIME))
+
+
+        try:
+            print("... Trying to enter username 'jdh'")
+
+            search_element = self.driver.driver.find_element(By.ID, 'username') # username
+            if search_element is None:
+                print("... Could not find username field")
+                return
+
+            search_element.send_keys('jdh')
+            sleep(1)
+            print("... Trying to enter password 'jdhpass'")
+
+            search_element = self.driver.driver.find_element(By.ID, 'password') # password
+            if search_element is None:
+                print("... Could not find username field")
+                return
+
+            search_element.send_keys('jdhpass')
+
+            sleep(1)
+            print("... Trying to click login")
+
+            sleep(random.randrange(MIN_WAIT_TIME, MAX_WAIT_TIME))
+            search_element = self.driver.driver.find_element(By.TAG_NAME, 'button') # login button
+            if search_element is None:
+                print("... Could not find login button")
+                return
+
+            ActionChains(self.driver.driver).move_to_element(search_element).click(search_element).perform()
+
+            sleep(random.randrange(MIN_WAIT_TIME, MAX_WAIT_TIME))
+        except:
+            print("... No login fields present, assuming we're already logged in")
+            pass
+
+        print("... Checking that secure page loaded")
+
+        search_element = self.driver.driver.find_element(By.XPATH, '/html/body/p') # main body paragraph
+        if search_element is None:
+            print("... Could not find body paragraph of secured page")
+            return
+
+        if 'example paragraph for a secure directory.' in search_element.text:
+            print("... Login successful!")
+        else:
+            print(f"... Login failed with: {search_element.text}")
+
+        
+
+        
 
 
