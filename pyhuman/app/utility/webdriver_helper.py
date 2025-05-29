9ca3c267baff0794b946e4c63e034e19fa46b573
Jason Hiser <jdhiser@gmail.com>
2025-05-29 09:03:42 -0400

    Update to use /tmp for chrome caching

--- pyhuman/app/utility/webdriver_helper.py
+++ pyhuman/app/utility/webdriver_helper.py
@@ -1,5 +1,12 @@
+import os
+import getpass
+
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
+from webdriver_manager.core.driver_cache import DriverCacheManager
+from webdriver_manager.core.manager import DriverManager
+from webdriver_manager.drivers.chrome import ChromeDriver
 
 from .base_driver import BaseDriverHelper
 
@@ -7,21 +14,38 @@ DRIVER_NAME = 'ChromeWebDriver'
 
 class WebDriverHelper(BaseDriverHelper):
 
-    options = webdriver.ChromeOptions()
-    options.add_argument("--disable-gpu")
-    options.add_argument('--ignore-certificate-errors')
-    options.add_argument("start-maximized")
-    options.add_argument("disable-infobars")
+
 
     def __init__(self):
         super().__init__(name=DRIVER_NAME)
-        #self._driver_path = ChromeDriverManager().install()
+        username = getpass.getuser()
+        user_tmp_dir = f"/tmp/chrome-profile-{username}"
+        cache_dir = os.path.join(user_tmp_dir, "wdm-cache")
+        os.makedirs(cache_dir, exist_ok=True)
+
+        # Force environment cache isolation
+        os.environ["WD_MANAGER_CACHE_PATH"] = cache_dir
+        os.environ["XDG_CACHE_HOME"] = os.path.join(user_tmp_dir, ".cache")
+        os.environ["XDG_CONFIG_HOME"] = os.path.join(user_tmp_dir, ".config")
+
+        # Set up Chrome options
+        self.options = webdriver.ChromeOptions()
+        self.options.add_argument(f'--user-data-dir={user_tmp_dir}')
+        self.options.add_argument('--disable-gpu')
+        self.options.add_argument('--ignore-certificate-errors')
+        self.options.add_argument('--start-maximized')
+        self.options.add_argument('--disable-infobars')
+
+        # Manually create cache manager
+        cache_manager = DriverCacheManager(cache_dir)
+        self._driver_path = ChromeDriverManager(cache_manager=cache_manager).install()
+
         self._driver = None
 
     @property
     def driver(self):
-        if self._driver == None:
-            self._driver = webdriver.Chrome(options=self.options)
+        if self._driver is None:
+            self._driver = webdriver.Chrome(service=Service(self._driver_path), options=self.options)
         return self._driver
 
     def stop_browser(self):
